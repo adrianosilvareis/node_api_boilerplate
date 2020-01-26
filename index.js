191bed39a25a6dd112af765da9b927fd2a83d1b4
Adriano Reis <adriano.reis@inflor.com.br>
2020-01-26 13:59:50 -0300

    adjust initial file for koa project

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const test = 'minha primeira string'
-console.log(test)
+import dotenv from 'dotenv'
+
+import app from './src/config/koa'
+
+dotenv.config() // load .env file, verify .env.sample
+
+const { HOST = 'localhost', PORT = 3000 } = process.env
+
+app.listen(PORT, () => console.info(`server listen on: http://${HOST}:${PORT}`))
